eb56f827d96b59c633d6e1e858d9b81c642e50da
ivanAnapa <vanya-cherniy@mail.ru>
2022-05-13 00:23:05 +0300

    dz4 updated for dz 6.2

--- src/test/java/BonusServiceTest.java
+++ src/test/java/BonusServiceTest.java
@@ -1,77 +1,16 @@
+package ru.netology.bonus;
 import org.junit.jupiter.api.Assertions;
-import org.junit.jupiter.api.Test;
+import org.junit.jupiter.params.ParameterizedTest;
+import org.junit.jupiter.params.provider.CsvFileSource;
 
 
 public class BonusServiceTest {
 
-    @Test
-    public void shouldCalculateWhenSmallAmountAndRegistered() {
+    @ParameterizedTest
+    @CsvFileSource(resources = {"/data1.csv"})
+    public void shouldCalculateWhenSmallAmountAndRegistered(boolean isRegistered, long amount, long expected) {
         BonusService service = new BonusService();
-
-        // подготавливаем данные:
-        long amount = 1000_60;
-        boolean registered = true;
-        long expected = 30;
-
-        // вызываем целевой метод:
-        long actual = service.calculate(amount, registered);
-
-        // производим проверку (сравниваем ожидаемый и фактический):
-        Assertions.assertEquals(expected, actual);
-    }
-
-    @Test
-    public void shouldCalculateWhenRegisteredAndOverLimit() {
-        BonusService service = new BonusService();
-
-        // подготавливаем данные:
-        long amount = 1_000_000_60;
-        boolean registered = true;
-        long expected = 500;
-
-        // вызываем целевой метод:
-        long actual = service.calculate(amount, registered);
-
-        // производим проверку (сравниваем ожидаемый и фактический):
-        Assertions.assertEquals(expected, actual);
-    }
-
-    @Test
-    public void shouldCalculateWhenSmallAmountAndUnregistered() {
-        BonusService bonusService = new BonusService();
-
-        long amount = 1_000_20;
-        boolean isRegistered = false;
-        long expected = 10;
-
-        long actual = bonusService.calculate(amount, isRegistered);
-
-        Assertions.assertEquals(expected, actual);
-    }
-
-    @Test
-    public void shouldCalculateWhenOverLimitAndUnregistered() {
-        BonusService bonusService = new BonusService();
-
-        long amount = 190_000_15;
-        boolean isRegistered = false;
-        long expected = 500;
-
-        long actual = bonusService.calculate(amount, isRegistered);
-
-        Assertions.assertEquals(expected, actual);
-    }
-
-    @Test
-    public void shouldCalculateWhenMinAmountAndUnregistered() {
-        BonusService bonusService = new BonusService();
-
-        long amount = 1;
-        boolean isRegistered = false;
-        long expected = 0;
-
-        long actual = bonusService.calculate(amount, isRegistered);
-
+        long actual = service.calculate(amount, isRegistered);
         Assertions.assertEquals(expected, actual);
     }
 
